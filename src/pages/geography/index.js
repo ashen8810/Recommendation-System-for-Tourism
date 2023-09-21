f57eb02818b4d13b1e0be951573b4b97b27082d8
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-09-21 18:53:03 +0530

    updated the geography page..

--- src/pages/geography/index.js
+++ src/pages/geography/index.js
@@ -12,7 +12,7 @@ const Geography = () => {
 
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="GEOGRAPHY" subtitle="The geological ttaffic." />
+      <Header title="GEOGRAPHY" subtitle="The geological traffic." />
       <Box
         mt="40px"
         height="75vh"
