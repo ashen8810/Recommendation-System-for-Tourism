49276b2f073151c299b722cc1069bcacd30e60b6
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-09-21 18:31:29 +0530

    added the routes to the dashboard.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       {/*<Schedule/>*/}
-      <Header />
+      {/* <Header /> */}
       
       <BrowserRouter>
       <ThemeProvider theme={theme}>
