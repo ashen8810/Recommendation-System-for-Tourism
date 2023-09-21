7bc920735025b4c0c82509728ed0c7f5e5dd6091
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-09-21 15:23:10 +0530

    added the profile image

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from 'components/Navbar'
+import Sidebar from 'components/Sidebar'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Dashboard
