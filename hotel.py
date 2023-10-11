7f1586b8425cc52ea9a03d4a2f36b71a2fd4f192
ashen8810 <ashen8810@gmail.com>
2023-10-11 16:36:10 +0530

    add hotels.py

---
+++ hotel.py
@@ -0,0 +1,138 @@
+import nltk
+nltk.download('wordnet')
+nltk.download("punkt")
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from ast import literal_eval
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+data = pd.read_csv("file1.csv")
+data = data.transpose()
+data = data.iloc[1:]
+
+data.columns = ["Name", "Url","Location","ratings","Features","Price","Description"]
+
+data.Location = data.Location.str.replace(", Sri Lanka", "")
+
+from bs4 import BeautifulSoup
+def column4(data):
+  d = []
+  soup = BeautifulSoup(data, 'lxml')
+  f = soup.findAll("li",{"class":"kml-col-12-12 kml-col-6-12-m"})
+  for i in f:
+    d.append(i.text)
+  return d
+data['Features'] = data['Features'].apply(column4)
+
+def column5(data):
+
+  d = []
+  try:
+    soup = BeautifulSoup(data, 'html.parser')
+
+    f = soup.findAll("div",{"class":"zV27-freebies"})
+
+    for i in f:
+      d.append(str(i.text).strip().split("\n"))
+  except:
+      d.append(["None"])
+
+  return d
+data['Description'] = data['Description'].apply(column5)
+
+data.drop(['Url',],1,inplace=True)
+
+
+
+
+
+
+data['Location'] = data['Location'].str.lower()
+
+
+
+import random
+guests = []
+for i in range(100):
+  guests.append(random.randint(a=1,b =5))
+
+
+
+data["Guests"] = guests
+
+
+dd = []
+for i in data['Features']:
+  k = []
+  for j in i:
+    j = j.strip()
+    k.append(j)
+  if len(k)==0:
+    k.append("Tea")
+
+  dd.append(",".join(k))
+data["Features"] = dd
+
+data.iat[31, 4] = 2264
+
+data["Price"] = data["Price"].str.replace("$","")
+data["Price"] = pd.to_numeric(data["Price"])
+
+sw = ['call', 'upon', 'still', 'nevertheless', 'down', 'every', 'forty', '‘re', 'always', 'whole', 'side', "n't", 'now', 'however', 'an', 'show', 'least', 'give', 'below', 'did', 'sometimes', 'which', "'s", 'nowhere', 'per', 'hereupon', 'yours', 'she', 'moreover', 'eight', 'somewhere', 'within', 'whereby', 'few', 'has', 'so', 'have', 'for', 'noone', 'top', 'were', 'those', 'thence', 'eleven', 'after', 'no', '’ll', 'others', 'ourselves', 'themselves', 'though', 'that', 'nor', 'just', '’s', 'before', 'had', 'toward', 'another', 'should', 'herself', 'and', 'these', 'such', 'elsewhere', 'further', 'next', 'indeed', 'bottom', 'anyone', 'his', 'each', 'then', 'both', 'became', 'third', 'whom', '‘ve', 'mine', 'take', 'many', 'anywhere', 'to', 'well', 'thereafter', 'besides', 'almost', 'front', 'fifteen', 'towards', 'none', 'be', 'herein', 'two', 'using', 'whatever', 'please', 'perhaps', 'full', 'ca', 'we', 'latterly', 'here', 'therefore', 'us', 'how', 'was', 'made', 'the', 'or', 'may', '’re', 'namely', "'ve", 'anyway', 'amongst', 'used', 'ever', 'of', 'there', 'than', 'why', 'really', 'whither', 'in', 'only', 'wherein', 'last', 'under', 'own', 'therein', 'go', 'seems', '‘m', 'wherever', 'either', 'someone', 'up', 'doing', 'on', 'rather', 'ours', 'again', 'same', 'over', '‘s', 'latter', 'during', 'done', "'re", 'put', "'m", 'much', 'neither', 'among', 'seemed', 'into', 'once', 'my', 'otherwise', 'part', 'everywhere', 'never', 'myself', 'must', 'will', 'am', 'can', 'else', 'although', 'as', 'beyond', 'are', 'too', 'becomes', 'does', 'a', 'everyone', 'but', 'some', 'regarding', '‘ll', 'against', 'throughout', 'yourselves', 'him', "'d", 'it', 'himself', 'whether', 'move', '’m', 'hereafter', 're', 'while', 'whoever', 'your', 'first', 'amount', 'twelve', 'serious', 'other', 'any', 'off', 'seeming', 'four', 'itself', 'nothing', 'beforehand', 'make', 'out', 'very', 'already', 'various', 'until', 'hers', 'they', 'not', 'them', 'where', 'would', 'since', 'everything', 'at', 'together', 'yet', 'more', 'six', 'back', 'with', 'thereupon', 'becoming', 'around', 'due', 'keep', 'somehow', 'n‘t', 'across', 'all', 'when', 'i', 'empty', 'nine', 'five', 'get', 'see', 'been', 'name', 'between', 'hence', 'ten', 'several', 'from', 'whereupon', 'through', 'hereby', "'ll", 'alone', 'something', 'formerly', 'without', 'above', 'onto', 'except', 'enough', 'become', 'behind', '’d', 'its', 'most', 'n’t', 'might', 'whereas', 'anything', 'if', 'her', 'via', 'fifty', 'is', 'thereby', 'twenty', 'often', 'whereafter', 'their', 'also', 'anyhow', 'cannot', 'our', 'could', 'because', 'who', 'beside', 'by', 'whence', 'being', 'meanwhile', 'this', 'afterwards', 'whenever', 'mostly', 'what', 'one', 'nobody', 'seem', 'less', 'do', '‘d', 'say', 'thus', 'unless', 'along', 'yourself', 'former', 'thru', 'he', 'hundred', 'three', 'sixty', 'me', 'sometime', 'whose', 'you', 'quite', '’ve', 'about', 'even']
+
+def cosine(l1,l2):
+  text1 = ' '.join(l1)
+  text2 = ' '.join(l2)
+
+  vectorizer = CountVectorizer().fit_transform([text1, text2])
+  cosine_sim = cosine_similarity(vectorizer)
+
+  similarity = cosine_sim[0][1]
+  return similarity
+
+def remove_punct(text):
+    cleaned_text = [re.sub(r'[^\w\s]', '', s) for s in text]
+    return cleaned_text
+
+def requirementbased(city,number,price,features):
+    data['Location']=data['Location'].str.lower()
+    features=features.lower()
+
+    features_tokens=word_tokenize(features)
+    sw = ['call', 'upon', 'still', 'nevertheless', 'down', 'every', 'forty', '‘re', 'always', 'whole', 'side', "n't", 'now', 'however', 'an', 'show', 'least', 'give', 'below', 'did', 'sometimes', 'which', "'s", 'nowhere', 'per', 'hereupon', 'yours', 'she', 'moreover', 'eight', 'somewhere', 'within', 'whereby', 'few', 'has', 'so', 'have', 'for', 'noone', 'top', 'were', 'those', 'thence', 'eleven', 'after', 'no', '’ll', 'others', 'ourselves', 'themselves', 'though', 'that', 'nor', 'just', '’s', 'before', 'had', 'toward', 'another', 'should', 'herself', 'and', 'these', 'such', 'elsewhere', 'further', 'next', 'indeed', 'bottom', 'anyone', 'his', 'each', 'then', 'both', 'became', 'third', 'whom', '‘ve', 'mine', 'take', 'many', 'anywhere', 'to', 'well', 'thereafter', 'besides', 'almost', 'front', 'fifteen', 'towards', 'none', 'be', 'herein', 'two', 'using', 'whatever', 'please', 'perhaps', 'full', 'ca', 'we', 'latterly', 'here', 'therefore', 'us', 'how', 'was', 'made', 'the', 'or', 'may', '’re', 'namely', "'ve", 'anyway', 'amongst', 'used', 'ever', 'of', 'there', 'than', 'why', 'really', 'whither', 'in', 'only', 'wherein', 'last', 'under', 'own', 'therein', 'go', 'seems', '‘m', 'wherever', 'either', 'someone', 'up', 'doing', 'on', 'rather', 'ours', 'again', 'same', 'over', '‘s', 'latter', 'during', 'done', "'re", 'put', "'m", 'much', 'neither', 'among', 'seemed', 'into', 'once', 'my', 'otherwise', 'part', 'everywhere', 'never', 'myself', 'must', 'will', 'am', 'can', 'else', 'although', 'as', 'beyond', 'are', 'too', 'becomes', 'does', 'a', 'everyone', 'but', 'some', 'regarding', '‘ll', 'against', 'throughout', 'yourselves', 'him', "'d", 'it', 'himself', 'whether', 'move', '’m', 'hereafter', 're', 'while', 'whoever', 'your', 'first', 'amount', 'twelve', 'serious', 'other', 'any', 'off', 'seeming', 'four', 'itself', 'nothing', 'beforehand', 'make', 'out', 'very', 'already', 'various', 'until', 'hers', 'they', 'not', 'them', 'where', 'would', 'since', 'everything', 'at', 'together', 'yet', 'more', 'six', 'back', 'with', 'thereupon', 'becoming', 'around', 'due', 'keep', 'somehow', 'n‘t', 'across', 'all', 'when', 'i', 'empty', 'nine', 'five', 'get', 'see', 'been', 'name', 'between', 'hence', 'ten', 'several', 'from', 'whereupon', 'through', 'hereby', "'ll", 'alone', 'something', 'formerly', 'without', 'above', 'onto', 'except', 'enough', 'become', 'behind', '’d', 'its', 'most', 'n’t', 'might', 'whereas', 'anything', 'if', 'her', 'via', 'fifty', 'is', 'thereby', 'twenty', 'often', 'whereafter', 'their', 'also', 'anyhow', 'cannot', 'our', 'could', 'because', 'who', 'beside', 'by', 'whence', 'being', 'meanwhile', 'this', 'afterwards', 'whenever', 'mostly', 'what', 'one', 'nobody', 'seem', 'less', 'do', '‘d', 'say', 'thus', 'unless', 'along', 'yourself', 'former', 'thru', 'he', 'hundred', 'three', 'sixty', 'me', 'sometime', 'whose', 'you', 'quite', '’ve', 'about', 'even']
+
+    lemm = WordNetLemmatizer()
+    f1_set = {w for w in features_tokens if not w in sw}
+    f_set=set()
+    for se in f1_set:
+        f_set.add(lemm.lemmatize(se))
+    reqbased=data[data['Location']==city.lower()]
+    reqbased=reqbased[reqbased['Guests']==number]
+    reqbased = reqbased[reqbased["Price"]<=price]
+    reqbased=reqbased.set_index(np.arange(reqbased.shape[0]))
+    l1 =[];l2 =[];cos=[];
+    for i in range(reqbased.shape[0]):
+        temp_tokens=word_tokenize(reqbased['Features'][i])
+        temp_tokens = [word.lower() for word in temp_tokens]
+        temp_tokens = remove_punct(temp_tokens)
+        temp1_set={w for w in temp_tokens if not w in sw}
+        temp_set=set()
+        for se in temp1_set:
+            temp_set.add(lemm.lemmatize(se))
+        # rvector = temp_set.intersection(f_set)
+
+        cos.append(cosine(temp_set,f_set))
+
+    reqbased['similarity']=cos
+    reqbased=reqbased.sort_values(by='similarity',ascending=False)
+    reqbased.drop_duplicates(subset='Name',keep='first',inplace=True)
+    return reqbased[['Name','Price',"Features",'similarity']].head(5)
+
+print(requirementbased('Kandy',2,2100,'i want Air conditioned room and a Coffee machine'))
+
