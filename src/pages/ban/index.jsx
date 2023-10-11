213b213a5cb039aca3302b1bd78a69a39392ff76
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-10-12 01:16:19 +0530

    added ban page

---
+++ src/pages/ban/index.jsx
@@ -0,0 +1,147 @@
+import { Box, Button, TextField, FormControl, InputLabel, Select, MenuItem} from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+
+const Ban = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="BAN USER" subtitle="Ban a User" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              }}
+            >
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name="firstName"
+                error={!!touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="lastName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 4" }}
+              />
+
+               
+                <TextField
+                fullWidth
+                variant="filled"
+                select
+                label="Duration"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.duration}
+                name="duration"
+                error={!!touched.duration && !!errors.duration}
+                helperText={touched.duration && errors.duration}
+                sx={{ gridColumn: "span 4" }}
+                >
+                <MenuItem value="2 weeks">2 Weeks</MenuItem>
+                <MenuItem value="1 months">1 Month</MenuItem>
+                <MenuItem value="3 months">3 Months</MenuItem>
+                
+                </TextField>
+
+
+                <TextField
+                fullWidth
+                variant="filled"
+                multiline
+                rows={4}
+                label="Reason (max 100 characters)"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.reason}
+                name="reason"
+                error={!!touched.reason && !!errors.reason}
+                helperText={touched.reason && errors.reason}
+                sx={{ gridColumn: "span 4" }}
+              />
+            </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Ban User
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  duration: yup.string().required("required"),
+  reason: yup.string().max(100, "Reason must be at most 100 characters").required("required")
+});
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address: "",
+  country: "",
+  duration: "",
+  reason: ""
+};
+
+export default Ban;
