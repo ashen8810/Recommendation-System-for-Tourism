8e8bb23c9a541cfa7103a03c08c7d2d50d39ba9a
Ashen Ranaweera <73464765+ashen8810@users.noreply.github.com>
2023-10-11 17:12:24 +0530

    Output the result

--- hotel.py
+++ hotel.py
@@ -23,7 +23,7 @@ data.Location = data.Location.str.replace(", Sri Lanka", "")
 from bs4 import BeautifulSoup
 def column4(data):
   d = []
-  soup = BeautifulSoup(data, 'html.parser')
+  soup = BeautifulSoup(data, 'lxml')
   f = soup.findAll("li",{"class":"kml-col-12-12 kml-col-6-12-m"})
   for i in f:
     d.append(i.text)
@@ -46,16 +46,9 @@ def column5(data):
   return d
 data['Description'] = data['Description'].apply(column5)
 
-data.drop(['Url',],1,inplace=True)
-
-
-
-
-
+data = data.drop(columns = ['Url'],axis = 1,inplace=False)
 
 data['Location'] = data['Location'].str.lower()
-
-
 
 import random
 guests = []
@@ -81,7 +74,7 @@ data["Features"] = dd
 
 data.iat[31, 4] = 2264
 
-data["Price"] = data["Price"].str.replace("$","")
+data["Price"] = data["Price"].str.replace('$',"",regex=False)
 data["Price"] = pd.to_numeric(data["Price"])
 
 sw = ['call', 'upon', 'still', 'nevertheless', 'down', 'every', 'forty', '‘re', 'always', 'whole', 'side', "n't", 'now', 'however', 'an', 'show', 'least', 'give', 'below', 'did', 'sometimes', 'which', "'s", 'nowhere', 'per', 'hereupon', 'yours', 'she', 'moreover', 'eight', 'somewhere', 'within', 'whereby', 'few', 'has', 'so', 'have', 'for', 'noone', 'top', 'were', 'those', 'thence', 'eleven', 'after', 'no', '’ll', 'others', 'ourselves', 'themselves', 'though', 'that', 'nor', 'just', '’s', 'before', 'had', 'toward', 'another', 'should', 'herself', 'and', 'these', 'such', 'elsewhere', 'further', 'next', 'indeed', 'bottom', 'anyone', 'his', 'each', 'then', 'both', 'became', 'third', 'whom', '‘ve', 'mine', 'take', 'many', 'anywhere', 'to', 'well', 'thereafter', 'besides', 'almost', 'front', 'fifteen', 'towards', 'none', 'be', 'herein', 'two', 'using', 'whatever', 'please', 'perhaps', 'full', 'ca', 'we', 'latterly', 'here', 'therefore', 'us', 'how', 'was', 'made', 'the', 'or', 'may', '’re', 'namely', "'ve", 'anyway', 'amongst', 'used', 'ever', 'of', 'there', 'than', 'why', 'really', 'whither', 'in', 'only', 'wherein', 'last', 'under', 'own', 'therein', 'go', 'seems', '‘m', 'wherever', 'either', 'someone', 'up', 'doing', 'on', 'rather', 'ours', 'again', 'same', 'over', '‘s', 'latter', 'during', 'done', "'re", 'put', "'m", 'much', 'neither', 'among', 'seemed', 'into', 'once', 'my', 'otherwise', 'part', 'everywhere', 'never', 'myself', 'must', 'will', 'am', 'can', 'else', 'although', 'as', 'beyond', 'are', 'too', 'becomes', 'does', 'a', 'everyone', 'but', 'some', 'regarding', '‘ll', 'against', 'throughout', 'yourselves', 'him', "'d", 'it', 'himself', 'whether', 'move', '’m', 'hereafter', 're', 'while', 'whoever', 'your', 'first', 'amount', 'twelve', 'serious', 'other', 'any', 'off', 'seeming', 'four', 'itself', 'nothing', 'beforehand', 'make', 'out', 'very', 'already', 'various', 'until', 'hers', 'they', 'not', 'them', 'where', 'would', 'since', 'everything', 'at', 'together', 'yet', 'more', 'six', 'back', 'with', 'thereupon', 'becoming', 'around', 'due', 'keep', 'somehow', 'n‘t', 'across', 'all', 'when', 'i', 'empty', 'nine', 'five', 'get', 'see', 'been', 'name', 'between', 'hence', 'ten', 'several', 'from', 'whereupon', 'through', 'hereby', "'ll", 'alone', 'something', 'formerly', 'without', 'above', 'onto', 'except', 'enough', 'become', 'behind', '’d', 'its', 'most', 'n’t', 'might', 'whereas', 'anything', 'if', 'her', 'via', 'fifty', 'is', 'thereby', 'twenty', 'often', 'whereafter', 'their', 'also', 'anyhow', 'cannot', 'our', 'could', 'because', 'who', 'beside', 'by', 'whence', 'being', 'meanwhile', 'this', 'afterwards', 'whenever', 'mostly', 'what', 'one', 'nobody', 'seem', 'less', 'do', '‘d', 'say', 'thus', 'unless', 'along', 'yourself', 'former', 'thru', 'he', 'hundred', 'three', 'sixty', 'me', 'sometime', 'whose', 'you', 'quite', '’ve', 'about', 'even']
@@ -134,5 +127,8 @@ def requirementbased(city,number,price,features):
     reqbased.drop_duplicates(subset='Name',keep='first',inplace=True)
     return reqbased[['Name','Price',"Features",'similarity']].head(5)
 
-print(requirementbased('Kandy',2,2100,'i want Air conditioned room and a Coffee machine'))
+res = requirementbased('Kandy',2,2100,'i want Air conditioned room and a Coffee machine')
+with open("result.txt","w") as f:
+   f.write(str(res))
+
 
