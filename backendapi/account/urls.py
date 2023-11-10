86a482a24999589d4007223979affe87971f219c
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-11-10 20:57:50 +0530

    added cahnge password functionality

--- backendapi/account/urls.py
+++ backendapi/account/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from account.views import  UserChangePasswordView, UserLoginView, UserProfileView, UserRegistrationView
+from account.views import (
+    UserChangePasswordView,
+    UserLoginView,
+    UserProfileView,
+    UserRegistrationView,
+    SendPasswordResetEmailView,
+)
+
 # from account.views import UserRegistrationView
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
-
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("profile/", UserProfileView.as_view(), name="profile"),
+    path("changepassword/", UserChangePasswordView.as_view(), name="changepassword"),
+    path(
+        "send-reset-password-email/",
+        SendPasswordResetEmailView.as_view(),
+        name="send-reset-password-email",
+    ),
 ]
--- backendapi/backendapi/settings.py
+++ backendapi/backendapi/settings.py
@@ -148,8 +148,8 @@ SIMPLE_JWT = {
 
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'userID',
-    'USER_ID_CLAIM': 'user_id',
+    'USER_ID_FIELD': 'userId',
+    'USER_ID_CLAIM': 'userId',
     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
