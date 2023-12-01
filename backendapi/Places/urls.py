a403e863fde29a1f31c69c4b03b291ee6d93f170
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-12-01 11:05:04 +0530

    added the search functionality

--- backendapi/Places/urls.py
+++ backendapi/Places/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from Places.views import  PlaceList,CommentList,ImageList
+from Places.views import PlaceList, CommentList, ImageList, PlaceSearchView
+
 # from account.views import UserRegistrationView
 urlpatterns = [
-    path('places/', PlaceList.as_view(), name='places'),
-    path('comments/', CommentList.as_view(), name='comments'),
-    path('images/', ImageList.as_view(), name='images'),
-
-
+    path("places/", PlaceList.as_view(), name="places"),
+    path("comments/", CommentList.as_view(), name="comments"),
+    path("images/", ImageList.as_view(), name="images"),
+    path("place/search/", PlaceSearchView.as_view(), name="place_search"),
 ]
