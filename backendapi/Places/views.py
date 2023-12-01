36d4891a332bf343c45f29fce50f7c2859b76dd1
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-12-01 11:05:36 +0530

    added the search functionality

--- backendapi/Places/views.py
+++ backendapi/Places/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Place,Comment,Image
-from .serializer import PlaceSerializer,CommentSerializer,ImageSerializer
+from .models import Place, Comment, Image
+from .serializer import PlaceSerializer, CommentSerializer, ImageSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 
 class PlaceList(APIView):
     def get(self, request):
@@ -16,23 +19,25 @@ class PlaceList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self, request, place_id):
-            try:
-                place = Place.objects.get(PlaceId=place_id)
-            except Place.DoesNotExist:
-                return Response(status=status.HTTP_404_NOT_FOUND)
 
-            serializer = PlaceSerializer(place, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+    def put(self, request, place_id):
+        try:
+            place = Place.objects.get(PlaceId=place_id)
+        except Place.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PlaceSerializer(place, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class CommentList(APIView):
     """
     List all comments or create a new comment.
     """
+
     def get(self, request):
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
@@ -45,10 +50,12 @@ class CommentList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class CommentDetail(APIView):
     """
     Retrieve, update, or delete a comment instance.
     """
+
     def get(self, request, comment_id):
         try:
             comment = Comment.objects.get(CommentId=comment_id)
@@ -69,7 +76,7 @@ class CommentDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 
 class ImageList(APIView):
     def get(self, request):
@@ -83,6 +90,7 @@ class ImageList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ImageDetail(APIView):
     def get(self, request, image_id):
@@ -105,3 +113,11 @@ class ImageDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PlaceSearchView(ListAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    # pagination_class = pageNumberPagination
+    filter_backends = SearchFilter, OrderingFilter
+    search_fields = ("PlaceName", "Category")
