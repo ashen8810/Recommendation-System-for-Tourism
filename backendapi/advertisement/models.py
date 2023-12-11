74c49968a7db977b2306dd1000da7917417ec5ee
Isuru <isurutharakabandara@gmail.com>
2023-12-11 22:00:04 +0530

    Added models in the advertisement app

--- backendapi/advertisement/models.py
+++ backendapi/advertisement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from account.models import User, Admin
 
-# Create your models here.
+class Advertisement(models.Model):
+    adId = models.CharField(max_length=5, primary_key=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    adminId = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    frequencyOfDisplay = models.IntegerField()
+    size = models.FloatField()
+    fee = models.FloatField()
+
+    def __str__(self):
+        return f"Ad ID: {self.adId}, User: {self.userId.userName}, Admin: {self.adminId.adminName}"
+
+class AdvertisementMedia(models.Model):
+    mediaID = models.CharField(max_length=7, primary_key=True)
+    adId = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    mediaType = models.CharField(max_length=10)
+    media = models.BinaryField()
+
+    def __str__(self):
+        return f"Media ID: {self.mediaID}, Ad ID: {self.adId.adId}, Type: {self.mediaType}"
