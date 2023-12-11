ee8a951768a6778f89b21bc7f995805e06231fd7
Isuru <isurutharakabandara@gmail.com>
2023-12-11 22:17:46 +0530

    update models in the place app

--- backendapi/Places/models.py
+++ backendapi/Places/models.py
@@ -1,40 +1,89 @@
-from django.db import models
+# from django.db import models
 
+
+# class Place(models.Model):
+#     PlaceId = models.AutoField(primary_key=True)
+#     UserId = models.CharField(max_length=10)  # fix this later
+#     AdminId = models.CharField(max_length=10)  # fix this later
+#     DateCreated = models.DateField()
+#     ContactNumber = models.CharField(max_length=10)
+#     PlaceName = models.CharField(max_length=250)
+#     CoordinateX = models.DecimalField(max_digits=2, decimal_places=1)
+#     CoordinateY = models.DecimalField(max_digits=2, decimal_places=1)
+#     Category = models.CharField(max_length=20)
+#     Website = models.CharField(max_length=250)
+#     OpeningHours = models.IntegerField()
+#     UserReviews = models.CharField(max_length=250)
+#     Description = models.CharField(max_length=250)
+#     IsUserUploaded = models.BooleanField()
+
+#     def __str__(self):
+#         return self.PlaceName
+
+
+# class Comment(models.Model):
+#     CommentId = models.AutoField(primary_key=True)
+#     UserId = models.CharField(max_length=10)  # fix this later
+#     AdminId = models.CharField(max_length=10)  # fix this later
+#     IsApproved = models.BooleanField()
+
+#     def __str__(self):
+#         return self.CommentId
+
+
+# class Image(models.Model):
+#     ImageId = models.AutoField(primary_key=True)
+#     PlaceId = models.ForeignKey(Place, on_delete=models.CASCADE)
+#     ImageUrl = models.CharField(max_length=250)
+
+#     def __str__(self):
+#         return self.ImageId
+
+
+from django.db import models
+from account.models import User, Admin
 
 class Place(models.Model):
-    PlaceId = models.AutoField(primary_key=True)
-    UserId = models.CharField(max_length=10)  # fix this later
-    AdminId = models.CharField(max_length=10)  # fix this later
-    DateCreated = models.DateField()
-    ContactNumber = models.CharField(max_length=10)
-    PlaceName = models.CharField(max_length=250)
-    CoordinateX = models.DecimalField(max_digits=2, decimal_places=1)
-    CoordinateY = models.DecimalField(max_digits=2, decimal_places=1)
-    Category = models.CharField(max_length=20)
-    Website = models.CharField(max_length=250)
-    OpeningHours = models.IntegerField()
-    UserReviews = models.CharField(max_length=250)
-    Description = models.CharField(max_length=250)
-    IsUserUploaded = models.BooleanField()
+    placeId = models.CharField(max_length=5, primary_key=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    adminId = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    createdDate = models.DateField()
+    contactNumber = models.CharField(max_length=10)
+    placeName = models.CharField(max_length=50)
+    coordinateX = models.IntegerField()
+    coordinateY = models.IntegerField()
+    category = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+    openingTime = models.TimeField()
+    closingTime = models.TimeField()
+    isUserUploaded = models.CharField(max_length=3)
+    description = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.PlaceName
+        return f"Place ID: {self.placeId}, Name: {self.placeName}, Category: {self.category}"
 
-
-class Comment(models.Model):
-    CommentId = models.AutoField(primary_key=True)
-    UserId = models.CharField(max_length=10)  # fix this later
-    AdminId = models.CharField(max_length=10)  # fix this later
-    IsApproved = models.BooleanField()
-
-    def __str__(self):
-        return self.CommentId
-
-
-class Image(models.Model):
-    ImageId = models.AutoField(primary_key=True)
-    PlaceId = models.ForeignKey(Place, on_delete=models.CASCADE)
-    ImageUrl = models.CharField(max_length=250)
+class ReviewPlace(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    placeId = models.ForeignKey(Place, on_delete=models.CASCADE)
+    review = models.CharField(max_length=255)
+    rating = models.IntegerField()
 
     def __str__(self):
-        return self.ImageId
+        return f"User ID: {self.userId.userId}, Place ID: {self.placeId.placeId}, Rating: {self.rating}"
+
+class PlaceComments(models.Model):
+    commentId = models.CharField(max_length=7, primary_key=True)
+    placeId = models.ForeignKey(Place, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+    isApproved = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"Comment ID: {self.commentId}, Place ID: {self.placeId.placeId}, Approved: {self.isApproved}"
+
+class PlaceImages(models.Model):
+    imageID = models.CharField(max_length=7, primary_key=True)
+    placeID = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.BinaryField()
+
+    def __str__(self):
+        return f"Image ID: {self.imageID}, Place ID: {self.placeID.placeId}"
