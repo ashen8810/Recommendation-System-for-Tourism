bef65ad2908138541dde7b94bf8fc9f918f7ada4
Isuru <isurutharakabandara@gmail.com>
2023-12-18 02:31:43 +0530

    Update About Us page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,73 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import './About.css';
 
 const About = () => {
-  
   return (
-    <div>
-        This is about us page !!!
+    <div className="about-us-container">
+      <h1 className='heading-tag'>About Our Wanderlust Community</h1>
+
+      <div className="about-us-content">
+        <p>
+          Welcome to <strong>Traveling Lanka</strong>, your gateway to unforgettable adventures and seamless travel experiences. Our platform is designed to assist, and connect travel enthusiasts from around the world.
+        </p>
+
+        <p>
+          At <strong>Traveling Lanka</strong>, we believe that every journey is an opportunity for discovery, and we are dedicated to making your travel dreams a reality. Here's what sets us apart:
+        </p>
+
+        <h3>Our Mission</h3>
+        <p>
+          Our mission is to empower travelers by providing personalized recommendations, fostering a community of explorers, and simplifying the travel planning process. We aim to be your go-to companion on every journey, ensuring each trip is as unique as you are.
+        </p>
+
+        <h3>What We Offer</h3>
+        
+            <ul>
+                <li>
+                 <strong>Travel Schedules: </strong> Easily generate personalized travel schedules tailored to your interests and preferences.
+                </li>
+                <li>
+                 <strong>Hotel Recommendations: </strong> Discover a curated list of accommodations to suit every budget and style.
+                </li>
+                <li>
+                 <strong>Explore Places: </strong> Find hidden gems and popular attractions in your chosen destination.
+                </li>
+                <li>
+                 <strong>Advertise Your Travel Services: </strong> Connect with fellow travelers by posting and discovering advertisements for travel-related services and experiences.
+                </li>
+
+            </ul>
+
+        
+
+        <h3>Our Team</h3>
+        <p>
+        <strong>Traveling Lanka</strong> is brought to you by a passionate team of travel enthusiasts who understand the thrill of exploration. We are committed to creating a platform that enhances your travel experiences and connects you with like-minded adventurers.
+        </p>
+
+        <h3>Contact Us</h3>
+        
+        <p>
+        Have questions, suggestions, or just want to share your latest travel story? We'd love to hear from you!
+        </p>
+
+          <ul>
+            <li>
+             Email: travelinglanka@gmail.com
+            </li>
+            <li>
+             Phone: +1 (555) 123-4567
+            </li>
+          </ul>
+
+        
+
+        <p>
+          Join our community, embark on your next adventure, and let [Your Website Name] be your trusted travel companion!
+        </p>
+      </div>
     </div>
   );
-};
+}
 
 export default About;
