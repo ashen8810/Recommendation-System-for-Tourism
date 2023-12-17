289a058f8a1cf430abcf0aed2538491a9160a06f
Isuru <isurutharakabandara@gmail.com>
2023-12-17 20:04:29 +0530

    Create FAQ page

---
+++ src/pages/FAQ/FAQ.jsx
@@ -0,0 +1,95 @@
+
+import React from 'react';
+import './FAQ.css';
+
+const FAQsPage = () => {
+    return (
+        <div className="faqs-container">
+            <h2 className='heading-tag'>Frequently Asked Questions</h2>
+
+            <div className="faq-section">
+                <h3>1. How can I create a travel schedule on your website?</h3>
+                <p>
+                    To create a travel schedule, navigate to Home page and click on the "Plane your trip" button. Follow the steps to input your preferences, and our system will generate a personalized itinerary for your trip.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>2. Is it mandatory to create a user profile to use your website?</h3>
+                <p>
+                    While you can browse hotel and tourist spots without a profile, creating one offers additional benefits such as creating schedule, saving your travel history, the ability to post advertisements, etc.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>3. How do I find hotels on your website?</h3>
+                <p>
+                    Use the "Hotel" feature on the homepage. Enter your destination, check-in/check-out dates, and preferences. Our system will then display a list of recommended hotels based on your criteria.        </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>4. How can I post an advertisement on your website?</h3>
+                <p>
+                    To post an advertisement, log in to your account and go to the "Contact us" section. Follow the instructions  and contact our admin.        </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>5. How can I post an advertisement on your website?</h3>
+                <p>
+                    To post an advertisement, log in to your account and go to the "Contact us" section. Follow the instructions  and contact our admin.        </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>6. Are there any fees associated with creating a user profile or using the travel schedule feature?</h3>
+                <p>
+                    No, creating a user profile and using the travel schedule feature are completely free of charge. We aim to provide valuable services to all our users without any hidden costs.
+                </p> </div>
+
+            <div className="faq-section">
+                <h3>7. Can I customize my travel schedule after it's generated?</h3>
+                <p>
+                    Absolutely! You can edit and customize your travel schedule as needed. Add or remove activities, change dates, and tailor it to your preferences.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>8. What should I do if I encounter issues with the website or need assistance?</h3>
+                <p>
+                    If you experience any issues or need assistance, please contact our support team at travelinglankasupport@gmail.com We are here to help and ensure you have a smooth experience.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>9.Can I share my travel schedule with friends and family?</h3>
+                <p>
+                    Yes, we will send you your schedule by mail. You can easily share your travel schedule among your friends and family.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>10. Can I save multiple travel schedules for different trips?</h3>
+                <p>
+                    Absolutely! Your profile allows you to save and organize multiple travel schedules for different trips. Access them easily and plan your future adventures with convenience.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>11. Is it possible to get personalized travel recommendations based on my interests?</h3>
+                <p>
+                    Yes, our system analyze your preferences, providing personalized recommendations for hotels and places to visit. The more you use the platform, the more tailored your suggestions become.
+                </p>
+            </div>
+
+            <div className="faq-section">
+                <h3>12. What safety measures are in place for user data and privacy?</h3>
+                <p>
+                    We take the security of user data seriously. Our website employs encryption protocols, and we adhere to strict privacy policies. For more details, please review our Privacy Policy.
+                </p>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default FAQsPage;
