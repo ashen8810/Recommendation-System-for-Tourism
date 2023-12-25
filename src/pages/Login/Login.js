da26893190c0c307e7458891e8faac9e32b47b5f
king-dilz <124921854+king-dilz@users.noreply.github.com>
2023-12-26 04:12:42 +0530

    integrated the frontend with backend

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,15 +2,34 @@ import React, { useState } from "react";
 import "./Login.css";
 import Google from "./google-logo.png";
 import Facebook from "./facebook-logo.png";
+import { toast } from 'react-toastify';
+import AxiosInstance from "../../utils/AxiosInstance.js";
+import {useNavigate} from 'react-router-dom'
+import axios from "axios";
 // import app from "./pages/Homepage";
 //import Register from "../../../../pages/Register/Register";
 
 export default function Login({ onClose }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
+  const navigate=useNavigate()
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [showOverlay, setShowOverlay] = useState(false);
+  const [error,setError] = useState("")
+  const [logindata, setLogindata]=useState({
+    email:"",
+    password:""
+   })
+
+   const {email,password} = logindata
+
+  
+  const handleOnchange=(e)=>{
+    setLogindata({...logindata, [e.target.name]:e.target.value})
+  }
+
 
   const handleLoginClick = () => {
     setShowOverlay(true);
@@ -48,6 +67,42 @@ export default function Login({ onClose }) {
     }
   };
 
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+   
+    if(!email || !password)
+    {
+      setError("all credentials are required.")
+    }else{
+      try{
+        const res = await axios.post('http://localhost:8000/api/user/login/', logindata)
+        const response= res.data
+        console.log(res)
+        if (res.status === 200) {
+          localStorage.setItem('access', JSON.stringify(response.token.access))
+          localStorage.setItem('refresh', JSON.stringify(response.token.refresh))
+          localStorage.setItem('user', JSON.stringify(response.user))
+          navigate('/')
+          toast.success('login successful')
+          onClose()
+        }
+      }catch(error){
+        console.error("An error occurred:", error);
+
+        // Access the error message from the response (if available)
+        if (error.response) {
+          console.error("Response error:", error.response.data);
+          const errorResponse = error.response.data;
+          // toast.error("Email or Password is not Valid");
+          toast.error(errorResponse.errors.non_field_errors[0]);
+        }
+      }
+      
+    }
+
+   
+ }
+
   return (
     <>
       {/* <Homepage /> */}
@@ -60,13 +115,16 @@ export default function Login({ onClose }) {
             &times;
           </span>
           <h2>Login</h2>
-          <form>
+          {error && <p className="error">{error}</p>}
+
+          <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
             <input
               type="email"
               id="email"
+              name="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleOnchange}
               onBlur={validateEmail}
             />
             {emailError && <p className="error">{emailError}</p>}
@@ -75,16 +133,16 @@ export default function Login({ onClose }) {
             <input
               type="password"
               id="password"
+              name="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleOnchange}
               onBlur={validatePassword}
             />
             {passwordError && <p className="error">{passwordError}</p>}
 
             <button
               className="logIn-button"
-              type="button"
-              onClick={handleLogin}
+              type="submit"
             >
               Log in
             </button>
