d735e84f5dd7ae4896f693f1a63e9e33a1159fc6
king-dilz <124921854+king-dilz@users.noreply.github.com>
2024-02-08 21:32:08 +0530

    added styling to verify email page

--- src/pages/verify/VerifyEmail.jsx
+++ src/pages/verify/VerifyEmail.jsx
@@ -10,15 +10,27 @@ const VerifyEmail = () => {
 
     const handleOtpSubmit = async(e)=>{
             e.preventDefault()
-            if (otp) {
+            try{
+              if (otp) {
                 const res = await axios.post('http://localhost:8000/api/user/verify-email/', {'otp':otp})
                 const resp = res.data
                 if (res.status === 200) {
                     navigate('/login')
                     toast.success(resp.message)
                 }
-                
+                else if(res.status === 204)
+                {
+                  toast.warn(resp.message)
+                }
+              }
             }
+            catch(error){
+                 if(error.response.status === 400)
+                 {
+                  toast.error(error.response.data.message)
+                 }
+            }
+          
             
     }
   return (
@@ -26,7 +38,7 @@ const VerifyEmail = () => {
         <div className='form-container'>
             <form action="" style={{width:"30%"}}  onSubmit={handleOtpSubmit}>
                <div className='form-group'>
-                 <label htmlFor="">Enter your Otp code:</label>
+                 <label htmlFor="">Enter your Otp code</label>
                  <input type="text"
                   className='email-form'  
                   name="otp"
@@ -34,7 +46,13 @@ const VerifyEmail = () => {
                   onChange={(e)=>setOtp(e.target.value)} 
                    />
                </div>
-               <button type='submit' className='vbtn'>Send</button>
+                <div className="button-container">
+                    <button type='submit' className='lbtn' onClick={()=>{
+                      {navigate('/login')}
+                    }}>Later</button>
+                    <button type='submit' className='vbtn'>Send</button>
+                </div>
+                  
             </form>
         </div>
     </div>
