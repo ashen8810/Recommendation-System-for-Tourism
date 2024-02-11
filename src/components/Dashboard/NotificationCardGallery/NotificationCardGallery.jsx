b0d1fd9e01f2852c86529e02cafd74877c89eaba
king-dilz <124921854+king-dilz@users.noreply.github.com>
2024-02-11 09:23:42 +0530

    integrated notification functionality to the frontend

--- src/components/Dashboard/NotificationCardGallery/NotificationCardGallery.jsx
+++ src/components/Dashboard/NotificationCardGallery/NotificationCardGallery.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState ,useEffect } from 'react'
 import { NotificationData } from '../../../data/mockData'
 import NotificationCard from '../NotificationCard/NotificationCard'
+import axios from 'axios'
+
 
 const NotificationCardGallery = () => {
+
+  const [NotificationData,setNotificationData]=useState([]);
+  useEffect(() => {
+    const fetchNotificationData = async () => {
+      
+
+    try {
+        
+          const response = await axios.get('http://localhost:8000/api/user/notification/')
+          console.log(response.data)
+          setNotificationData(response.data.results);
+         
+    } catch (error) {
+        console.error('Error fetching user data:', error.message);
+    }
+    };
+
+    fetchNotificationData() 
+}, []);
+
   return (
     <div className="container" >
       {
-           NotificationData.map((item,index)=>(
-             <NotificationCard item={item}/>
+            NotificationData && NotificationData.map((item,index)=>(
+             <NotificationCard item={item.content}/>
            ))
+          
+           
       }
+
+      {console.log(NotificationData)}
         
     </div>
   )
